0a98d5826488f0304e415a4ff99c98c7b3b1517b
liefu zhang <liefuzhangnz@gmail.com>
2017-11-25 17:42:29 +1300

    Exercise 2

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers {
+    public class CustomersController : Controller {
+
+        // GET: Customer
+        public ActionResult Index() {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id) {
+            var customer = GetCustomers().SingleOrDefault((c) => { return c.Id == id; });
+            
+            if (customer == null) {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers() {
+            return new List<Customer> {
+                new Customer {Id=1, Name="John Smith" },
+                new Customer {Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
