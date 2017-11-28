d6e6bde76dee95216ebe27ee6407c64c1e466201
liefu zhang <liefuzhangnz@gmail.com>
2017-11-28 22:51:29 +1300

    Validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,11 +8,12 @@ namespace Vidly.Models {
     public class Customer {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsLetter { get; set; }
